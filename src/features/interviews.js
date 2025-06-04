cde4d7f7c9750be96fbf4de54ab7f47dc45756e6
DemitriBr <75550776+DemitriBr@users.noreply.github.com>
2025-06-04 12:58:36 -0500

    Create interviews.js

---
+++ src/features/interviews.js
@@ -0,0 +1,10 @@
+// src/features/interviews.js
+// Interview scheduling functionality placeholder
+export function initializeInterviews() {
+    console.log('Interviews feature initialized');
+}
+
+export function addInterview(applicationId, interviewData) {
+    console.log('Adding interview:', applicationId, interviewData);
+    // TODO: Implement interview functionality
+}
